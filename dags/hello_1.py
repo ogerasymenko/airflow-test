9a7028524ded3da147867799309fbecd93589676
Oleksandr Herasymenko <oleksandr.herasymenko@loopme.com>
2020-04-07 17:21:34 +0300

    Update 6

--- dags/hello_1.py
+++ dags/hello_1.py
@@ -3,26 +3,21 @@ from airflow.utils.dates import days_ago
 from datetime import datetime, timedelta, date
 from airflow.contrib.operators.kubernetes_pod_operator import KubernetesPodOperator
 
-today = str(date.today())
-arr = [int(x) for x in today.split('-')]
-
-year = arr[0]
-month = arr[1]
-day = arr[2]
+today = str(date.today()).replace('-', '.')
 
 default_args = {
     'owner': 'airflow',
     'depends_on_past': False,
-    'start_date': datetime(year, month, day),
+    'start_date': datetime.strptime(today, '%Y.%m.%d'),
     'retries': 2,
     'retry_delay': timedelta(minutes=5)
 }
 
 dag = DAG(
-    'k8s-example',
+    dag_id='k8s-example',
     default_args=default_args,
     description='A simple tutorial DAG',
-    schedule_interval="*/15 * * * *"
+    schedule_interval="@hourly"
 )
 
 k8s_example_task_1 = KubernetesPodOperator(
@@ -32,6 +27,7 @@ k8s_example_task_1 = KubernetesPodOperator(
     arguments=["date"],
     name="k8s-example-task-1",
     task_id="k8s-example-task-1",
+    startup_timeout_seconds=10,
     get_logs=True,
     dag=dag,
     in_cluster=True,
@@ -45,6 +41,7 @@ k8s_example_task_2 = KubernetesPodOperator(
     cmds=["java", "--version"],
     name="k8s-example-task-2",
     task_id="k8s-example-task-2",
+    startup_timeout_seconds=10,
     get_logs=True,
     dag=dag,
     in_cluster=True,
