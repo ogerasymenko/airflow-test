23b5a51e274de9ecafd8b8fa50b58d27ceb3ab79
Oleksandr Herasymenko <oleksandr.herasymenko@loopme.com>
2020-04-08 12:23:32 +0300

    Update 8

--- dags/hello_1.py
+++ dags/hello_1.py
@@ -31,8 +31,8 @@ k8s_example_task_1 = KubernetesPodOperator(
     get_logs=True,
     dag=dag,
     in_cluster=True,
-    do_xcom_push=False,
-    is_delete_operator_pod=True
+    do_xcom_push=True,
+    is_delete_operator_pod=False
 )
 
 k8s_example_task_2 = KubernetesPodOperator(
@@ -45,8 +45,8 @@ k8s_example_task_2 = KubernetesPodOperator(
     get_logs=True,
     dag=dag,
     in_cluster=True,
-    do_xcom_push=False,
-    is_delete_operator_pod=True
+    do_xcom_push=True,
+    is_delete_operator_pod=False
 )
 
 k8s_example_task_2.set_upstream(k8s_example_task_1)
